a63526bc012111c960416018f63442637cc715e9
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2020-01-29 23:37:43 -0500

    basic site structure

---
+++ template/header.php
@@ -0,0 +1,10 @@
+<header class="header">
+    <h1 class="hidden">Main Navigation</h1>
+    <a class="logo"><img src="images/logo.svg" alt="logo"></a>
+    <i class="fa fa-bars"></i>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</header>
